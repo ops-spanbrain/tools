662c0993e802d4120d238ca7273ca1a30ac536ea
andy <andy@gmail.com>
2021-11-06 18:03:50 +0400

    fix

---
+++ inginx.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+
+tee /etc/yum.repos.d/nginx.repo <<-'EOF'
+[nginx]
+name=nginx repo
+baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
+gpgcheck=0
+enabled=1
+EOF
+
+yum -y install nginx
+
+rm -rf /etc/nginx/nginx.conf
+
+tee /etc/nginx/nginx.conf <<-'EOF'
+user  nginx;
+worker_processes  auto;
+
+error_log  /var/log/nginx/error.log warn;
+pid        /var/run/nginx.pid;
+
+worker_rlimit_nofile 65535;
+
+events{
+  use epoll;
+  worker_connections 65535;
+}
+
+
+http{
+  include       /etc/nginx/mime.types;
+  default_type  application/octet-stream;
+
+  sendfile on;
+  client_header_timeout 15;
+  client_body_timeout 15;   
+  client_max_body_size 50m;
+  send_timeout    600;
+  keepalive_timeout 60;
+  autoindex off;
+  server_tokens off;
+
+  gzip on;
+  gzip_min_length  1k;
+  gzip_buffers     4 16k;
+  gzip_http_version 1.0;
+  gzip_comp_level 2;
+  gzip_types       text/plain application/x-javascript text/css application/xml;
+  gzip_vary on;
+  gzip_disable msie6;
+  log_format '$remote_addr - $remote_user [$time_local] "$request" '
+                '$status $body_bytes_sent "$http_referer" '
+                '"$http_user_agent" "$http_x_forwarded_for"';
+  include /etc/nginx/conf.d/*;
+}
+EOF
+
+
+
