d0e6f9194cfb98a8fe2539c944e89c6ba30e0e2c
andy <andy@gmail.com>
2021-11-15 09:45:45 +0400

    update openssl

--- init.sh
+++ init.sh
@@ -21,6 +21,28 @@ echo "LANG=en_US.UTF-8" >> /etc/environment
 echo "LC_ALL=en_US.UTF-8" >> /etc/environment
 
 
+cd /usr/lcoal/src/
+mv /usr/bin/openssl /usr/bin/openssl.bak
+mv /usr/include/openssl /usr/include/openssl.bak
+
+wget -O https://www.openssl.org/source/openssl-1.1.1k.tar.gz
+
+tar -zxvf openssl-1.1.1k.tar.gz
+
+cd /usr/local/openssl-1.1.1k
+
+./config --prefix=/usr/local/openssl
+make && make install
+
+ln -s /usr/local/openssl/bin/openssl /usr/bin/openssl
+ln -s /usr/local/openssl/include/openssl /usr/include/openssl
+echo "/usr/local/openssl/lib" >> /etc/ld.so.conf
+ldconfig -v
+
+openssl version
+
+
+
 yum -y install ntp
 systemctl enable ntpd 
 systemctl start ntpd 
